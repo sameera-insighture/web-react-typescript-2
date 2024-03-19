0d90e1aba5a277faacbedb23f575b25d97b867db
sameera-insighture <sameeera.l@insighture.com>
2024-03-19 14:53:38 +0530

    Add Init Code

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import "./App.css";
+import Dashboard from "./components/Dashboard";
+
+function App() {
+  return (
+    <div className="App">
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
